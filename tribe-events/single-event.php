0ba3ca9eff7b307426edab4da9bf63c1d0648420
TCCoding <gdsc@tccoding.com>
2017-06-08 18:59:27 -0400

    1

--- tribe-events/single-event.php
+++ tribe-events/single-event.php
@@ -216,11 +216,13 @@ $done = array();
 			});		
 			
 			<?php
-			if(isset($_REQUEST['acf-field_5939ced2dcd39'])){
-				if(is_array($_REQUEST['acf-field_5939ced2dcd39'])){
+			if(isset($_REQUEST['acf'])){
+				if(is_array($_REQUEST['acf']['field_5939ced2dcd39'])){
 					if($_REQUEST['acf-field_5939ced2dcd39'][1]  == 1){
 						?> alert('Terminé!'); <?php
 					}  
+				}else{
+					?> alert('<?php echo $_REQUEST['acf']['field_5939ced2dcd39']; ?>'); <?php
 				}
 			}
 			?>
