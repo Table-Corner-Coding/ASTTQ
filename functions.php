75775008896c31ed101e1f4d977ff22c39b5dbfb
TCCoding <gdsc@tccoding.com>
2017-08-01 00:37:16 -0400

    ds

--- functions.php
+++ functions.php
@@ -798,7 +798,7 @@ function get_points_table_for_event($event_id, $refresh = false){
 				$tireur_id = $competiteur['tireur'];
 				$tireur = get_post($tireur_id);
 				$vehicule = get_field('nom_du_vehicule', $tireur_id);
-				$nom_tireur = $tireur->post_title;
+				$nom_tireur = get_field('nom_du_profil', $tireur_id);
 				
 				$distances = $competiteur['distances'];
 				
