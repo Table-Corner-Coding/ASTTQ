14002984f2826af5dec46c64735428821dc799b4
TCCoding <gdsc@tccoding.com>
2017-08-02 23:56:32 -0400

    ad

--- includes/tcc.js
+++ includes/tcc.js
@@ -204,9 +204,13 @@ jQuery(document).ready(function(){
 				//alert('Tireur: '+tireursNom[index]+' ('+item+')');	
 				
 				addButton.trigger('click');
+				
+				var nomTireur = tireursNom[index];
+				
 				setTimeout(function(){
+					
 						table.find('tbody tr:nth-child('+index+') td[data-name=tireur] input[type=hidden]').val(item);
-						table.find('tbody tr:nth-child('+index+') td[data-name=tireur] span.select2-chosen').html(tireursNom[index]);
+						table.find('tbody tr:nth-child('+index+') td[data-name=tireur] span.select2-chosen').html(nomTireur);
 						table.find('tbody tr:nth-child('+index+') td[data-name=tireur] input[type=hidden]').trigger('change');
    					}, 200);
 			});
