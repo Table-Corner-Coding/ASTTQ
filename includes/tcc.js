9b2d63e2a1a87a93a1b1787bdb3c675d97434676
TCCoding <gdsc@tccoding.com>
2017-08-02 23:28:54 -0400

    das

--- includes/tcc.js
+++ includes/tcc.js
@@ -205,8 +205,8 @@ jQuery(document).ready(function(){
 			
 			tireursArray.forEach(function(item, index){
 				
-				table.find('tbody tr:nth-child(index) td[data-name=tireur] input[type=hidden]').val(item);
-				table.find('tbody tr:nth-child(index) td[data-name=tireur] span.select2-chosen').html(tireursNom[index]);
+				table.find('tbody tr:nth-child('+index+') td[data-name=tireur] input[type=hidden]').val(item);
+				table.find('tbody tr:nth-child('+index+') td[data-name=tireur] span.select2-chosen').html(tireursNom[index]);
 				
 			});
 			
