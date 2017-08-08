2aeca3c407c9f5a526b1f4267658b309544b70c7
TCCoding <gdsc@tccoding.com>
2017-08-08 17:27:41 -0400

    gdf

--- includes/tcc.js
+++ includes/tcc.js
@@ -220,7 +220,7 @@ jQuery(document).ready(function(){
 				var nomTireur = tireursNom[index];
 				var trueIndex = index;
 				
-				
+				/*
 				setTimeout(function(){
 					
 						table.find('tbody tr:nth-child('+trueIndex+') td[data-name=tireur] input[type=hidden]').val(item);
@@ -230,7 +230,7 @@ jQuery(document).ready(function(){
 						//table.find('tbody tr:nth-child('+trueIndex+') td[data-name=tireur] span.select2-chosen').html(nomTireur);
 						table.find('tbody tr:nth-child('+trueIndex+')').attr('title',item);
    					}, 200);
-				
+				*/
 			});
 			
 			
