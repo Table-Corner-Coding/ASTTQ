9a466353a6ef01d48e767e0c22a83acc1b41928b
TCCoding <gdsc@tccoding.com>
2017-08-10 15:46:05 -0400

    asd

--- functions.php
+++ functions.php
@@ -1297,7 +1297,7 @@ function edition_competitions_shortcode() {
 
 
 
-							$tabs .= '<tr class="add_tireur_line"><td colspan="4"><span class=\'dashicons dashicons-plus-alt\'></span></td></tr></tbody></table> </form>';
+							$tabs .= '</tbody><tfoot><tr class="add_tireur_line"><td colspan="5"><span class=\'dashicons dashicons-plus-alt\'></span></td></tr></tfoot></table> </form>';
 							
 							$tireurArray = array();
 							$tireursNoms = array();
