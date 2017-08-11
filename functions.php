a8ed21917ca9af9dfe12bb539e2d7dfed5baf077
TCCoding <gdsc@tccoding.com>
2017-08-11 17:07:38 -0400

    dg

--- functions.php
+++ functions.php
@@ -1219,7 +1219,7 @@ function edition_competitions_shortcode() {
 						
 						$tabs .= '[et_pb_accordion_item title="'.$classe->name.'"]';
 						
-						$tabs .= '<a class="sButton" href="" data-icon="">Sauvegarder</a>';
+						$tabs .= '<a class="sButton" data-icon="">Sauvegarder</a>';
 						$tabs .= '<form id="form_edition_'.$classe->term_id.'">';
 							$tabs .= '<table data-event-id="'.$event->ID.'" data-term-id="'.$classe->term_id.'" class="editable_table comp_table"><thead><tr><th><span title="Mélanger le tableau" class="dashicons dashicons-randomize"></span></th><th>'.__('Véhicule','asttq').'</th><th>'.__('Conducteur','asttq').'</th><th>'.__('Distances','asttq').'</th><th>'.__('Membre','asttq').'</th><th>Actions</th></tr></thead><tbody>';
 						if(!empty($compArr[$objID])){
