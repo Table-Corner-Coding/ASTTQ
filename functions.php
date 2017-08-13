2d938c80c2a201c040131c7143805c3f9d1573c1
TCCoding <gdsc@tccoding.com>
2017-08-13 17:31:12 -0400

    yr

--- functions.php
+++ functions.php
@@ -1217,7 +1217,10 @@ function edition_competitions_shortcode() {
 						$tabs .= '<form id="form_edition_'.$classe->term_id.'">';
 						$tabs .= '<a class="sButton" data-icon="">Sauvegarder</a>';
 							$tabs .= '<table data-event-id="'.$event->ID.'" data-term-id="'.$classe->term_id.'" class="editable_table comp_table"><thead><tr><th><span title="Mélanger le tableau" class="dashicons dashicons-randomize"></span></th><th>'.__('Véhicule','asttq').'</th><th>'.__('Conducteur','asttq').'</th><th>'.__('Distances','asttq').'</th><th>'.__('Membre','asttq').'</th><th>Actions</th></tr></thead><tbody>';
-						if(!empty($compArr[$objID])&& !empty($compArr[$objID]['competiteur'])){
+						if(!empty($compArr[$objID])){
+							
+							/* La compétition existe déjà! */
+							
 							$allTireurs = $compArr[$objID]['competiteur'];
 							$allNonMembres = $compArr[$objID]['non-membre'];
 							
