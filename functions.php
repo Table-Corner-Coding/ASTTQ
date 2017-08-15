fadf3e9330bcbcdc193344e3be6fc7bf2a3f00ad
TCCoding <gdsc@tccoding.com>
2017-08-15 01:09:21 -0400

    dfg

--- functions.php
+++ functions.php
@@ -1367,6 +1367,8 @@ function edition_competitions_shortcode() {
 							$bonus = 0;
 						}
 						
+						
+						
 						$tabs .= '<div class="bonus"><strong>Bonus de points</strong><input type="number" class="bonus_points" value="'.$bonus.'" /></div>';
 							$tabs .= '<table data-event-id="'.$event->ID.'" data-term-id="'.$classe->term_id.'" class="editable_table comp_table"><thead><tr><th><span title="Mélanger le tableau" class="dashicons dashicons-randomize"></span></th><th>'.__('Véhicule','asttq').'</th><th>'.__('Conducteur','asttq').'</th><th>'.__('Distances','asttq').'</th><th>'.__('Membre','asttq').'</th><th>Actions</th></tr></thead><tbody>';
 						
@@ -1376,6 +1378,8 @@ function edition_competitions_shortcode() {
 							
 							$allTireurs = $compArr[$objID]['competiteur'];
 							$allNonMembres = $compArr[$objID]['non-membre'];
+							
+							$tabs .= ' <!-- Membres: '.print_r($allTireurs,true).' --> ';
 							
 							$ordered_table = array();
 							
